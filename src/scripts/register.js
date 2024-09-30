351fd086aff684d1f71d08c2571442166ab3dda5
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2024-09-30 07:43:34 -0300

    Email set

--- src/scripts/register.js
+++ src/scripts/register.js
@@ -22,11 +22,12 @@ const passwordInput = document.querySelector("#password");
 form.onsubmit = e => {
   e.preventDefault();
   createUserWithEmailAndPassword(auth, emailInput.value, passwordInput.value).then(credentials => {
-    auth.currentUser.updateProfile({ displayName: nameInput.value.trim() });
     set(ref(db, 'usuarios/' + credentials.user.uid), {
       name: nameInput.value.trim(),
+      email: emailInput.value.trim(),
     });
     console.log(credentials);
+    window.location.href = '/src/pages/login.html';
   }).catch(error => {
     alert("Não foi posssível fazer o cadastro.");
     console.log(error.code);
