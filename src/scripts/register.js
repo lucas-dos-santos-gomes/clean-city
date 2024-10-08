f95fd332055dd7f553b72aa34b5d33bcdaf83853
Lucas dos Santos Gomes <lucas.gomes@telefonica.com>
2024-10-08 16:08:23 -0300

    Correct Login and Register

--- src/scripts/register.js
+++ src/scripts/register.js
@@ -1,15 +1,10 @@
 import {
   db,
   set,
-  get,
   ref,
-  child,
-  push,
-  onChildAdded,
-  createUserWithEmailAndPassword,
   auth,
-  dbref,
-  signInWithEmailAndPassword,
+  authError,
+  createUserWithEmailAndPassword,
   updateProfile,
   signOut,
 } from "./config.js";
@@ -18,25 +13,48 @@ const form = document.querySelector("form");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
-const btn = document.querySelector("button");
+const confirmPasswordInput = document.querySelector("#confirm-password");
+const submitButton = document.querySelector("button");
 
-form.onsubmit = e => {
-  e.preventDefault();
-  btn.setAttribute('disabled', true);
-  createUserWithEmailAndPassword(auth, emailInput.value.trim(), passwordInput.value).then(credentials => {
-    set(ref(db, 'users/' + credentials.user.uid), {
+form.onsubmit = signup;
+
+function signup(submit) {
+  submit.preventDefault();
+  turnInputs(true);
+
+  createUserWithEmailAndPassword(auth, emailInput.value.trim(), passwordInput.value).then(async credentials => {
+    await set(ref(db, 'users/' + credentials.user.uid), {
       name: nameInput.value.trim(),
       email: emailInput.value.trim(),
       password: passwordInput.value,
       admin: false,
     });
-    btn.removeAttribute('disabled');
+    await updateProfile(auth.currentUser, { displayName: nameInput.value.trim() });
+    signOut(auth);
+
+    turnInputs();
     alert('Cadastro realizado com sucesso!');
     window.location.href = '/src/pages/login.html';
   }).catch(error => {
-    alert("Não foi posssível fazer o cadastro.");
+    alert(authError(error.message));
     console.log(error);
-    console.log(error.message);
-    btn.removeAttribute('disabled');
-});
+    turnInputs();
+  });
+}
+
+function turnInputs(disabled) {
+  if(disabled) {
+    nameInput.setAttribute('disabled', true);
+    emailInput.setAttribute('disabled', true);
+    passwordInput.setAttribute('disabled', true);
+    confirmPasswordInput.setAttribute('disabled', true);
+    submitButton.setAttribute('disabled', true);
+    return;
+  }
+
+  nameInput.removeAttribute('disabled');
+  emailInput.removeAttribute('disabled');
+  passwordInput.removeAttribute('disabled');
+  confirmPasswordInput.removeAttribute('disabled');
+  submitButton.removeAttribute('disabled');
 }
